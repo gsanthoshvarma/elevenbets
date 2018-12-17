44ed010aa9b2cc7b954f3ae6093c644f62e40e54
gsanthoshvarma <santhoshvarma.g@gmail.com>
2018-12-17 20:13:32 +0530

    removed unwanted packages

--- src/main/java/com/tech/online/dao/SMusersDAOImpl.java
+++ src/main/java/com/tech/online/dao/SMusersDAOImpl.java
@@ -4,7 +4,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
-import org.hibernate.cfg.Configuration;
 
 import com.tech.online.po.SMusersPO;
 import com.tech.online.poimpl.SMusersPOlmpl;
