660e9b010f83b5ff0d698ad5a268eb7ad0d8398d
gsanthoshvarma <santhoshvarma.g@gmail.com>
2018-12-19 02:59:45 +0530

    modified method for multi thread accessing

--- src/main/java/com/tech/online/util/HibernateUtil.java
+++ src/main/java/com/tech/online/util/HibernateUtil.java
@@ -1,5 +1,36 @@
 package com.tech.online.util;
 
+import java.io.ObjectStreamException;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+/**
+ * 
+ * @author GVS
+ * Singleton class to get one SeesionFactory object per one application/jvm
+ */
 public class HibernateUtil {
 
+	private static volatile SessionFactory sessionFactory = null;
+	
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			synchronized(HibernateUtil.class) {
+				if(sessionFactory == null) 
+					return sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+			}
+		}
+			return sessionFactory;
+	}
+	
+	private Object readResolve() throws ObjectStreamException {
+		return sessionFactory;
+	}
+	
+	private HibernateUtil() {
+		super();
+		if(sessionFactory != null) {
+			throw new RuntimeException("to create SessionFactory please use getInstance() method");
+		}
+	}
 }
