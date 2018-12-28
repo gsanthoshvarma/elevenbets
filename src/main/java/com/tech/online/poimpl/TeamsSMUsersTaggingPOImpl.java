89232243288484b4eeba2d90d9b93d90b6627e8e
sandyG416 <sandeep.sm541@gmail.com>
2018-12-28 19:55:17 +0530

    test

--- src/main/java/com/tech/online/poimpl/TeamsSMUsersTaggingPOImpl.java
+++ src/main/java/com/tech/online/poimpl/TeamsSMUsersTaggingPOImpl.java
@@ -2,7 +2,10 @@ package com.tech.online.poimpl;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 /**
@@ -15,7 +18,9 @@ public class TeamsSMUsersTaggingPOImpl {
 	public TeamsSMUsersTaggingPOImpl() {
 		super();
 	}
-    @Id
+	@Id
+    @SequenceGenerator(name="seq",sequenceName="tb_mst_smusers_tagging_seq_id")        
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq") 
 	private long id;
     
     @Column(name="team_id")
@@ -24,6 +29,17 @@ public class TeamsSMUsersTaggingPOImpl {
     
     @Column(name="is_active")
 	private int isActive;
+    
+    @Column(name="username")
+    private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 	public long getId() {
 		return id;
